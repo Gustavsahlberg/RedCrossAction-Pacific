a813e5e86620418ddf14db9b1bfbe53ac191bbe9
pontusgranath <pontus.granath@learnet.se>
2020-10-08 14:04:56 +0200

    Sends a mail to every address in DB

--- website/volunteer/admin.py
+++ website/volunteer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
+from .models import AddMail
 # Register your models here.
+admin.site.register(AddMail)
--- website/volunteer/views.py
+++ website/volunteer/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 from django.conf import settings
 
+from .models import AddMail
 
 def index(request):
 
     if request.method == 'POST':
         message = request.POST['message']
-        reciever = request.POST['reciever']
 
+        recievers = []
+        for mail in AddMail.objects.all():
+            recievers.append(mail.mail_address)
 
-        send_mail('Mail for help',
-            message, 
-            settings.EMAIL_HOST_USER, 
-            [reciever], 
-            fail_silently=False)
-    return render(request, 'volunteer/index.html')
+        send_mail('Mail for help', message, settings.EMAIL_HOST_USER, recievers)
+
+        # send_info2 = ('Mail for help',
+        #     message, 
+        #     settings.EMAIL_HOST_USER, 
+        #     [reciever2], 
+        #     )
+
+        # datatuple = (
+        #     ('Mail for Help', message, settings.EMAIL_HOST_USER, [reciever]),
+        #     ('Mail for Order', message, settings.EMAIL_HOST_USER, [reciever2]),
+        # )
+        # send_mass_mail(datatuple)
+        # send_mass_mail((send_info1, send_info2), fail_silently=False)
+
+        
+    latest_mail = AddMail.objects.order_by('-pub_date')[:5]
+    context = {'latest_mail': latest_mail}
+    return render(request, 'volunteer/index.html', context)
     
